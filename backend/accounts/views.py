33652fc6c8ff300d1002cef1b21571d3bbe426ab
ijustwannathat <kokfbc@gmail.com>
2025-07-20 20:38:01 +0200

    user handling is changed that only authenticated user can see his data and admin can see all data

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,18 +1,34 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 
 from .models import User
 from .serializers import UserSerializer
 
 
+# yea this thing def needs a viewset
 class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return (
+            super().get_queryset()
+            if user.is_staff
+            else super().get_queryset().filter(id=user.id)
+        )
 
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        user = self.request.user
+        return (
+            super().get_queryset()
+            if user.is_staff
+            else super().get_queryset().filter(id=user.id)
+        )
