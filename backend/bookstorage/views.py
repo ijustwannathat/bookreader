6e6e2bb34fb73f4747596bf1bd555d6e6e874ecd
ijustwannathat <kokfbc@gmail.com>
2025-07-22 15:25:26 +0200

    views commit added

--- backend/bookstorage/views.py
+++ backend/bookstorage/views.py
@@ -11,20 +11,20 @@ from .serializers import BookSeriazlier
 class BookView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSeriazlier
-    permission_classes = [IsAuthor]
+    # temporary measure that as i thought would work for basic logic, i didnt focus on writing permissions explicitly
+    # permission_classes = [IsAuthor]
     parser_classes = [MultiPartParser, FormParser]
 
-    # ah sorry didnt want to write viewsets so gotta copy paste, such a simple logic does not need routers anyways
-    # TODO: write a viewset and set the router up
+    # TOFIX: with planned filtering and stuff this shit definitely needs a router
     # def perform_create(self, serializer):
-        # file = self.request.FILES.get("file")
-        #
-        # if file:
-        #     instance = EpubHandler(file=file)
-        #     handled_data = instance.handle_file()
-        #     serializer.save(**handled_data)
-        # else:
-        # serializer.save(added_by=self.request.user)
+    # file = self.request.FILES.get("file")
+    #
+    # if file:
+    #     instance = EpubHandler(file=file)
+    #     handled_data = instance.handle_file()
+    #     serializer.save(**handled_data)
+    # else:
+    # serializer.save(added_by=self.request.user)
 
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
