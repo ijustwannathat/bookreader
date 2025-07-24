481f7f3a44f5a258227bd1403150d06716daf9a2
ijustwannathat <kokfbc@gmail.com>
2025-07-24 21:42:27 +0200

    proper testing added, free books were tested and now awaiting assertions for further improvement, also files are now being handled properly with RAM not being overthrown by the might of free <tripledigit> books

--- backend/bookstorage/tests.py
+++ backend/bookstorage/tests.py
@@ -1,17 +1,21 @@
 import os
+import shutil
 
+from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
 from accounts.models import User, get_token_for_user
 
+path = "books/"
+
 
 class TestBook(APITestCase):
     def setUp(self) -> None:
         email = "tomandjarry@gmail.com"
         password = "banish12345"
-        d = "http://127.0.0.1:8000/api/token/"
+        d = reverse("token_obtain_pair")
         self.user_instance = User.objects.create_user(
             id=224, email=email, password=password
         )
@@ -21,28 +25,45 @@ class TestBook(APITestCase):
 
         self.usr = self.client.post(d, {"email": email, "password": password})
         self.token = self.usr.json().get("access")
+        self.test_files_path = os.path.join(settings.MEDIA_ROOT, "user_224")
 
     def upload_file(self, filepath: str, token: str):
         with open(filepath, "rb") as f:
             content = f.read()
-            url = reverse("book-view")
 
-            response = self.client.post(
-                url,
-                {
-                    "file": SimpleUploadedFile(
-                        content=content,
-                        name=os.path.basename(path),
-                        content_type="epub+zip",
-                    ),
-                    "title": "1",
-                    "author": "1",
-                    "format": "epub",
-                },
-                headers={"Authorization": f"Bearer {token}"},
+        url = reverse("book-view")
+
+        response = self.client.post(
+            url,
+            {
+                "file": SimpleUploadedFile(
+                    content=content,
+                    name=os.path.basename(filepath),
+                    content_type="epub+zip",
+                ),
+                "title": "1",
+                "author": "1",
+                "format": "epub",
+            },
+            headers={"Authorization": f"Bearer {token}"},
+        )
+
+        return response
+
+    def test_file_uploads(self):
+        files = os.listdir(path)
+        for file in files:
+            response = self.upload_file(filepath=path + file, token=self.token)
+            print(response.status_code)
+        else:
+            print(
+                "----------------------------------------------------------------------"
             )
-            return response
+            print()
+        print(f"Run {len(files)} files. OK")
 
-    def test_file_upload(self):
-        result = self.upload_file(filepath=path, token=self.token)
-        print(result)
+    def tearDown(self):
+        try:
+            shutil.rmtree(str(self.test_files_path))
+        except OSError as e:
+            print(e)
